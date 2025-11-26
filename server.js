63c82a8f258799ea05820f87d1146a3a76149672
Bruno Costa <obrnocsta@gmail.com>
2025-11-26 09:07:16 -0300

    feat: adiciona servidor http porta 8000, e responde string html

---
+++ server.js
@@ -0,0 +1,17 @@
+import http from 'node:http'
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end(`
+      <html>
+        <h1>The server is working</h1>
+      </html>
+    `)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}`)
+})
