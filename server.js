b993c8151c565f0ae7c6061113ad57f4c6ab3013
Bruno Costa <obrnocsta@gmail.com>
2025-11-27 08:37:38 -0300

    feat: adiciona 'data/data.json' e função de leitura 'utils/getData.js'

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
+import { getData } from './utils/getData.js'
 
 const PORT = 8000
 const __dirname = import.meta.dirname
+const data = await getData(__dirname)
 
 const server = http.createServer(async (req, res) => {
   await serveStatic(req, res, __dirname)
