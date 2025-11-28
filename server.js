5511afcdb706d6d6fdf7ca7c109d85e4919ff5ec
Bruno Costa <obrnocsta@gmail.com>
2025-11-28 19:28:17 -0300

    feat: implementa função 'handleNews()'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
-import { handleGet, handlePost } from './handlers/routeHandlers.js'
+import { handleGet, handlePost, handleNews } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 const __dirname = import.meta.dirname
@@ -13,6 +13,10 @@ const server = http.createServer(async (req, res) => {
   if (req.url === '/api' && req.method === 'POST') {
 
     return handlePost(req, res)
+  }
+
+  if (req.url === "/api/news" && req.method === 'GET') {
+    return await handleNews(req, res)
   }
 
   if (!req.url.startsWith('/api')) {
