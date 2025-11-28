b2edd425a56d628f836a6c855b219460eb48da20
Bruno Costa <obrnocsta@gmail.com>
2025-11-28 09:50:47 -0300

    feat: implementa 'addNewStighting()', le e adiciona o body do POST no 'data.json'

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,13 +1,21 @@
 import { getData } from '../utils/getData.js'
 import { sendResponse } from '../utils/sendResponse.js'
 import { parseJSONBody } from '../utils/parseJSONBody.js'
+import { addNewSighting } from '../utils/addNewSighting.js'
 
-export const handleGet = async (res, dirname) => {
-  let data = await getData(dirname)
+export const handleGet = async (res) => {
+  let data = await getData()
   data = JSON.stringify(data)
   sendResponse(res, 200, 'appllication/json', data)
 }
 
 export const handlePost = async (req, res) => {
-  const parsedBody = await parseJSONBody(req)
+  try {
+    const parsedBody = await parseJSONBody(req)
+    await addNewSighting(parsedBody)
+    sendResponse(res, 201, 'application/json', JSON.stringify(parsedBody))
+
+  } catch (error) {
+    sendResponse(res, 400, 'application/json', JSON.stringify({ error: error }))
+  }
 }
