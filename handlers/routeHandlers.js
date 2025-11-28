7d682ea0b1c5bb51a416b1c0500a148487657edd
Bruno Costa <obrnocsta@gmail.com>
2025-11-27 21:51:26 -0300

    feat: adiciona função 'handlePost', implementa o path '/api' da rota 'POST'

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -6,3 +6,23 @@ export const handleGet = async (res, dirname) => {
   data = JSON.stringify(data)
   sendResponse(res, 200, 'appllication/json', data)
 }
+
+export const handlePost = async (req, res) => {
+
+  const buffers = []
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const data = Buffer.concat(buffers).toString()
+
+  let body = []
+  try {
+    body = JSON.parse(data)
+  } catch (error) {
+    body = data
+    console.log(error)
+  }
+
+  console.log(body)
+}
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
-import { handleGet } from './handlers/routeHandlers.js'
+import { handleGet, handlePost } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
   if (req.url === '/api' && req.method === 'GET') {
-    await handleGet(res, __dirname)
+    return await handleGet(res, __dirname)
+  }
+
+  if (req.url === '/api' && req.method === 'POST') {
+
+    return handlePost(req, res)
   }
 
   if (!req.url.startsWith('/api')) {
