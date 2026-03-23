d51027c55a86e67670ce116f7b4690d96c48247d
Bruno Costa <obrnocsta@gmail.com>
2026-03-23 02:40:02 -0300

    doc: adiciona arquivos iniciais para implementar a página de notícias na rota '/api/news'

--- public/news.js
+++ public/news.js
@@ -1,15 +1,17 @@
-const eventSource = new EventSource("http://localhost:8000/api/news");
+const eventSource = new EventSource("/api/news") 
 
-const liveContainer = document.getElementById("live-container");
+const liveContainer = document.getElementById("live-container")
 
-// Handle live price updates
+
+// Handle live price updates 
 eventSource.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-  const story = data.story;
-  liveContainer.textContent = story;
-};
+  const data = JSON.parse(event.data)
+  const story = data.story
+  liveContainer.textContent = story
+}
 
 // Handle connection loss
 eventSource.onerror = () => {
-  console.log("Connection lost. Attempting to reconnect...");
-};
+  console.log("Connection lost. Attempting to reconnect...")
+}
+
